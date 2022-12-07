38580a225bf20aaee591bf905fc835be5f4e220b
Abhi-coder722 <109688594+Abhi-coder722@users.noreply.github.com>
2022-12-07 23:58:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fileInput = document.querySelector('.file-input'),
     previewImg = document.querySelector(".preview-img img"),
     chooseImgBtn = document.querySelector(".choose-img"),
     resetFilterBtn = document.querySelector(".reset-filter");
+    saveImgBtn = document.querySelector(".save-img");
 
 let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
 let rotate = 0, flipHorizontal = 1, flipVertical = 1;
@@ -93,14 +94,28 @@ rotateOptions.forEach(option => {
 });
 
 const resetFilter = () => {
-    //resetting everythin to its default value
+    //resetting everything to its default value
     brightness = 100; saturation = 100; inversion = 0; grayscale = 0;
     rotate = 0; flipHorizontal = 1; flipVertical = 1;
     filterOptions[0].click(); //clicking brightness btn , so the brightness will be clicked when we reset the image 
     applyFilters();
 }
 
+const saveImage= () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width=previewImg.naturalWidth;
+    canvas.height=previewImg.naturalHeight;
+
+    ctx.filter=`brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+    ctx.scale(flipHorizontal,flipVertical);
+    ctx.translate(canvas.width/2,canvas.height/2);
+
+    ctx.drawImage(previewImg,-canvas.width/2,-canvas.height/2,canvas.width,canvas.height);
+    document.body.appendChild(canvas);
+}
 fileInput.addEventListener("change", loadImage);
 filterSlider.addEventListener("input", updateFilter);
 resetFilterBtn.addEventListener("click", resetFilter);
+saveImgBtn.addEventListener("click", saveImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
