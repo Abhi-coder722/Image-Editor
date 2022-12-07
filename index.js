0cf33802b3605e7d51a99d4bc3c4f4b80171b495
Abhi-coder722 <109688594+Abhi-coder722@users.noreply.github.com>
2022-12-07 21:34:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const fileInput=document.querySelector('.file-input'),
+filterOptions=document.querySelectorAll(".filter button");
+filterName=document.querySelector(".filter-info .name"),
+filterValue=document.querySelector(".filter-info .value"),
+filterSlider=document.querySelector(".slider input"),
+previewImg=document.querySelector(".preview-img img"),
 chooseImgBtn=document.querySelector(".choose-img");
 
-chooseImgBtn.addEventListener("click", () => fileInput.click())
+const loadImage= () => {
+    let file = fileInput.files[0]; //getting the user selected files
+    if(!file) return; // return if user hasn't chosen any file
+    previewImg.src = URL.createObjectURL(file); //passing file url as the source
+    previewImg.addEventListener("load", () => {
+        document.querySelector(".container").classList.remove("disable");
+    });
+}
+
+filterOptions.forEach(option =>{
+    option.addEventListener("click", () => { //adding the click event listener to all the buttons
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+        filterName.innerText = option.innerText;
+    })
+});
+
+const updateFilter = () => {
+    console.log(filterSlider.value);
+    var t=filterSlider.value;
+    filterValue.innerText= t+'%';
+}
+fileInput.addEventListener("change",loadImage);
+filterSlider.addEventListener("input",updateFilter);
+chooseImgBtn.addEventListener("click", () => fileInput.click());
